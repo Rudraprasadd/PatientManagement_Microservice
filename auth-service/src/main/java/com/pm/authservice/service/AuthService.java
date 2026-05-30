c49dfc0cba736f6e1b9592f4701ba92e8637cfca
Rudraprasad Satapathy <rudraprasadsatapathy3506@gmail.com>
2026-05-30 19:13:37 +0530

    Auth Service Added

---
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -0,0 +1,33 @@
+package com.pm.authservice.service;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    //password request -> password -> encoded -> encoded pass
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(U -> passwordEncoder.matches(LoginRequestDTO.getPassword(),
+                        U.getPassword()))
+                .map(U -> jwtUtil.generateToken(U.getEmail(),U.getRole()));
+
+        return token;
+    }
+}
